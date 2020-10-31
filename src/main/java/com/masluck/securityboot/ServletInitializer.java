a1c7fdce91403e6a1aea03efcce2b13e32cb020d
maslak_andrey <masluck@gmail.com>
2020-10-31 22:51:24 +0500

    Start to coding, clear MVC app

---
+++ src/main/java/com/masluck/securityboot/ServletInitializer.java
@@ -0,0 +1,13 @@
+package com.masluck.securityboot;
+
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+
+public class ServletInitializer extends SpringBootServletInitializer {
+
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(SecuritybootApplication.class);
+	}
+
+}
